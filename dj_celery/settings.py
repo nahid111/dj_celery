9d2a778b1707ae658c49944e4fd1522753aeeef4
Muhammad Nahid <mdnahid22@gmail.com>
2023-05-14 14:56:50 +0600

    startapp

--- dj_celery/settings.py
+++ dj_celery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dj_celery_app',
 ]
 
 MIDDLEWARE = [
---
+++ dj_celery_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjCeleryAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dj_celery_app'
