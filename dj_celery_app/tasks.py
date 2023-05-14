fd537c7d187a43b609004f8d7a683e2270a7e5f9
Muhammad Nahid <mdnahid22@gmail.com>
2023-05-15 00:27:20 +0600

    Get result by id. Update template

--- dj_celery_app/tasks.py
+++ dj_celery_app/tasks.py
@@ -1,4 +1,6 @@
 from celery import shared_task
+from celery.result import AsyncResult
+from dj_celery import celery_app
 import time
 
 
@@ -6,3 +8,10 @@ import time
 def mock_task(num=15):
     time.sleep(num)
     return f"Slept for {num} seconds"
+
+
+def get_result_by_id(task_id):
+    res = AsyncResult(task_id, app=celery_app)
+    if res.status is not "PENDING":
+        return res.get()
+    return None
--- dj_celery_app/views.py
+++ dj_celery_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .tasks import mock_task
+from .tasks import mock_task, get_result_by_id
+from django.conf import settings
 
 
 def index(request):
-    res = mock_task.delay()
-    print(res.id)
-    return render(request, 'index.html')
+    msg = None
+    if settings.CURRENT_TASK_ID is None:
+        tsk = mock_task.delay(5)
+        settings.CURRENT_TASK_ID = tsk.id
+    else:
+        msg = get_result_by_id(settings.CURRENT_TASK_ID)
+        if msg:
+            settings.CURRENT_TASK_ID = None
+    return render(request, 'index.html', {"msg": msg})
